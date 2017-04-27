ce7fa419abd6263eb10b92cbdbddd407ac9038cd
Hugo Dias <hugomrdias@gmail.com>
2017-04-27 14:25:43 +0100

    remove import/no-unassigned-import from react

--- react.js
+++ react.js
@@ -5,6 +5,7 @@ module.exports = {
 
     rules: {
         'react/jsx-no-bind': 2,
-        'react/no-did-update-set-state': 2
+        'react/no-did-update-set-state': 2,
+        'import/no-unassigned-import': 0
     }
 };
