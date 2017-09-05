b7ddd2cb1aa495019dbaec3767ee9f89887755a7
Hugo Dias <hugomrdias@gmail.com>
2017-09-05 17:22:05 +0100

    feat: add fixtures to sort-imports rule

---
+++ fixtures/sort-import.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Range from './../utils/range';
+import * as DateUtils from './../utils/i18n/datetime';
+import '../vendor/datepicker';
+import NotificationsBadge from './notifications-badge';
+
+React.cloneElement();
+
+const t = Range;
+
+t.f();
+
+DateUtils.e();
+const n = new NotificationsBadge();
+
+n();
